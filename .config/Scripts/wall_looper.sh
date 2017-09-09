7dff7a28c524a268d34a845ad8a04054e338e68b
psfloyd <peter.racoon@gmail.com>
2017-09-08 23:59:49 -0300

    Wallpaper and grive Loop

---
+++ .config/Scripts/wall_looper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Feed this script two arguments, $1 for a folder directory and $2 for a time interval in seconds.
+
+#Will loop thru the files in the directory, using feh to change the background every $2 seconds.
+
+while true; do
+	for file in "$1"*;	do
+		sleep $2
+		feh --bg-scale "$file"
+		cp "$file" ~/.config/wall.png
+	done
+done
