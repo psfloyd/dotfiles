43b2d16fcf58824b6b18599b23a26f6b8f9b1c40
psfloyd <peter.racoon@gmail.com>
2017-09-11 23:49:30 -0300

    Shortcuts.py working as intended. Ranger still floppy

--- .config/Scripts/shortcuts.py
+++ .config/Scripts/shortcuts.py
@@ -1,54 +1,52 @@
 import csv
+import os
+
+home = os.getenv("HOME");
+os.chdir(home);
 
 # qute = ""
 rang = ""
-bash = ""
-zzsh = ""
+alsh = ""
 
 # with open(".config/qutebrowser/keys.conf.base") as qb:
     # qute+=qb.read()
 with open(".config/ranger/rc.conf.base") as rg:
     rang+=rg.read()
-with open(".config/Scripts/bashrc") as bsh:
-    bash+=bsh.read()
-with open(".config/Scripts/zshrc.local.base") as zsh:
-    zzsh+=zsh.read()
+with open(".config/Scripts/aliases_sh.base") as ash:
+    alsh+=ash.read()
 
-#First we open the list of folder shortcuts and go down each line adding each in the required syntax to each of the three configs:
-
+rang+=("# cd Aliases{{{"+"\n")
+alsh+=("# cd Aliases{{{"+"\n")
 with open(".config/Scripts/folders") as fold:
     for line in csv.reader(fold, dialect="excel-tab"):
         # Adds the qutebrowser downloads commands:
         # qute+="set storage download-directory "+line[1]+" ;; hint links download\n\t;"+line[0]+"\n"
-
         #Adds the ranger go, tab, move and yank commands:
         rang+=("map g"+line[0]+" cd "+line[1]+"\n")
         rang+=("map t"+line[0]+" tab_new "+line[1]+"\n")
         rang+=("map m"+line[0]+" shell mv %s "+line[1]+"\n")
         rang+=("map Y"+line[0]+" shell cp -r %s "+line[1]+"\n")
+        #Adds the alsh shortcuts:
+        alsh+=("alias cd"+line[0]+"=\"cd "+line[1]+" && ls -a\"\n")
 
-        #Adds the bash shortcuts:
-        bash+=("alias "+line[0]+"=\"cd "+line[1]+" && ls -a\"\n")
+rang+=("# }}}"+"\n"+"\n")
+alsh+=("# }}}"+"\n"+"\n")
+#Goes thru the config file file and adds the shortcuts to both alsh and ranger.
 
-        #Adds the zsh shortcuts:
-        zzsh+=("alias "+line[0]+"=\"cd "+line[1]+" && ls -a\"\n")
-
-#Goes thru the config file file and adds the shortcuts to both bash and ranger.
+rang+=("# Config Aliases{{{"+"\n")
+alsh+=("# Config Aliases{{{"+"\n")
 
 with open(".config/Scripts/configs") as conf:
     for line in csv.reader(conf, dialect="excel-tab"):
         rang+=("map "+line[0]+" shell vim "+line[1]+"\n")
+        alsh+=("alias "+line[0]+"=\"vim "+line[1]+"\"\n")
 
-        bash+=("alias "+line[0]+"=\"vim "+line[1]+"\"\n")
-
-        zzsh+=("alias "+line[0]+"=\"vim "+line[1]+"\"\n")
-
+rang+=("# }}}"+"\n"+"\n")
+alsh+=("# }}}"+"\n"+"\n")
 
 # with open(".config/qutebrowser/keys.conf","w") as outqb:
     # outqb.write(qute)
 with open(".config/ranger/rc.conf", "w") as outrang:
     outrang.write(rang)
-with open(".bashrc","w") as outbash:
-    outbash.write(bash)
-with open(".zshrc.local","w") as outzzsh:
-    outzzsh.write(zzsh)
+with open(".aliases_sh","w") as outalsh:
+    outalsh.write(alsh)
