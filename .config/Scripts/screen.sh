451421930c3e7b5e3377189a529971206c9599f9
psfloyd <peter.racoon@gmail.com>
2017-09-15 03:04:14 -0300

    Added Screen Mode

--- .config/Scripts/screen.sh
+++ .config/Scripts/screen.sh
@@ -2,23 +2,33 @@
 
 #Feed this script either:
 #	"l" for laptop screen only,
-#	"v" for vga screen only,
+#	"e" for external screen only,
 #	or "d" for dual vga/laptop.
 
-d() { if [[ $(xrandr -q | grep VGA1\ con) ]]
-	then param $1
-	else echo "No VGA input detected."
-	fi ;}
-dual() { xrandr --output LVDS1 --auto --output VGA1 --auto --right-of LVDS1 ;}
-laptop() { xrandr --output LVDS1 --auto --output VGA1 --off ;}
-vga() { xrandr --output VGA1 --auto --output LVDS1 --off ;}
-#mirror() { xrandr --addmode VGA1 $lapres && xrandr --output LVDS1 --mode $lapres --output VGA1 --mode $lapres ;}
+extdisp=""
+
+main() { if [[ $(xrandr -q | grep HDMI1\ con) ]]; then
+	extdisp="HDMI1"	
+	param $1
+elif [[ $(xrandr -q | grep VGA1\ con) ]]; then
+	extdisp="VGA1"	
+	param $1
+else echo "No HDMI/VGA input detected."
+fi ;}
+
+dual() { xrandr --output LVDS1 --auto --primary --output $extdisp --auto --right-of LVDS1 ;}
+
+laptop() { xrandr --output LVDS1 --auto --output $extdisp --off ;}
+
+ext() { xrandr --output $extdisp --auto --output LVDS1 --off ;}
+
+#mirror() { xrandr --addmode $extdisp $lapres && xrandr --output LVDS1 --mode $lapres --output $extdisp --mode $lapres ;}
 
 param() {
 case $1 in
 	d) dual ;;
-	v) vga ;;
+	e) ext ;;
 	l) laptop ;;
 	*) echo -e "Invalid parameter. Add one of the following:\n\"d\" for dualscreen laptop and VGA.\n\"l\" for laptop only\n\"v\" for VGA only." ;;
 esac ;}
-d $1
+main $1
