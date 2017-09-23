bda8fcf28daf9d9eeed4f294fdd9056fd2420e5f
psfloyd <peter.racoon@gmail.com>
2017-09-22 23:41:04 -0300

    Compton toggle

--- .config/i3/exec.sh
+++ .config/i3/exec.sh
@@ -3,30 +3,37 @@
 term=termite
 
 case "$1" in
-	tmux)
+tmux)
 
-		if i3-msg -t get_tree | jq -r . | grep -q "dropdown"; then
-		i3-msg '[instance="dropdown"] scratchpad show; move position center'
+if i3-msg -t get_tree | jq -r . | grep -q "dropdown"; then
+	i3-msg '[instance="dropdown"] scratchpad show; move position center'
 
+else
+	i3-msg '--' exec --no-startup-id "$term --name dropdown -e 'tmux new-session -A -s Bgp'"
+	sleep .12s
+	i3-msg '[instance="dropdown"] scratchpad show; move position center'
+fi	
+	;;
+music)
+	if i3-msg -t get_tree | jq -r . | grep -q "ncmpcppw"; then
+		i3-msg '--' \
+			'[instance="ncmpcppw"] scratchpad show',\
+			'[instance="ncmpcppw"] fullscreen enable'
 	else
-		i3-msg '--' exec --no-startup-id "$term --name dropdown -e 'tmux new-session -A -s Bgp'"
+		i3-msg '--' exec --no-startup-id "$term --name ncmpcppw -e 'ncmpcpp'"
 		sleep .12s
-		i3-msg '[instance="dropdown"] scratchpad show; move position center'
-	fi	
-		;;
-	music)
-		if i3-msg -t get_tree | jq -r . | grep -q "ncmpcppw"; then
-			i3-msg '--' \
-				'[instance="ncmpcppw"] scratchpad show',\
-				'[instance="ncmpcppw"] fullscreen enable'
-		else
-			i3-msg '--' exec --no-startup-id "$term --name ncmpcppw -e 'ncmpcpp'"
-			sleep .12s
-			i3-msg '--' \
-				'[instance="ncmpcppw"] scratchpad show',\
-				'[instance="ncmpcppw"] fullscreen enable'
-		fi
-		;;
-	*)
-		exit
+		i3-msg '--' \
+			'[instance="ncmpcppw"] scratchpad show',\
+			'[instance="ncmpcppw"] fullscreen enable'
+	fi
+	;;
+compton)
+	if [ `pgrep compton` ]; then
+		killall compton
+	else
+		compton &
+	fi
+	;;
+*)
+	exit
 esac
