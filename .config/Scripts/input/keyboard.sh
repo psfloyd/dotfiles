4f4ce8c0d61382db302a2f37e77ee1c82989b2ab
psfloyd <peter.racoon@gmail.com>
2017-12-20 23:20:33 -0300

    Moved keyboard and touchpad to input

--- .config/Scripts/input/keyboard.sh
+++ .config/Scripts/input/keyboard.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Set layout
 setxkbmap -layout latam -option caps:swapescape -option caps:ctrl_modifier
 
+# Preserve Escape
 xmodmap -e 'keycode 255 = Escape'
 
-xcape -e '#66=Escape' -d 1>/dev/null
-
+# Caps press as escape and space press as space
+xcape -e "#66=Escape" -d 1>/dev/null
